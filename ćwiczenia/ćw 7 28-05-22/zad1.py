37181fbd55cfdb3b3f093855730ca11bcbf240ca
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-05 02:17:03 +0200

    ex7/zad1 przedzialy jednostkowe

--- ćwiczenia/ćw 7 28-05-22/zad1.py
+++ ćwiczenia/ćw 7 28-05-22/zad1.py
@@ -1,2 +1,9 @@
-#todo
-
+def przedzialy(T):
+    T.sort()
+    curr = (0, 0)
+    res = []
+    for i in T:
+        if i > curr[1]:
+            res.append((i, i+1))
+            curr = (i, i+1)
+    return res
