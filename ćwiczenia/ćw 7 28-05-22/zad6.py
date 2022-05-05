dfaf5de30cd0730911ef540df9ad753095729aea
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-05 02:31:49 +0200

    ex7/zad6 suma odleglosci

--- ćwiczenia/ćw 7 28-05-22/zad6.py
+++ ćwiczenia/ćw 7 28-05-22/zad6.py
@@ -1,2 +1,29 @@
 #todo
+def find_spot(A):
+    def partition(A, p, r):
+        x = A[r]
+        i = p - 1
+        for j in range(p, r):
+            if A[j] <= x:
+                i += 1
+                A[i], A[j] = A[j], A[i]
+        A[i + 1], A[r] = A[r], A[i + 1]
+        return i + 1
 
+    def select(A, p, k, r):
+        if p == r:
+            return A[p]
+        if p < r:
+            q = partition(A, p, r)
+            if q == k:
+                return A[q]
+            elif q < k:
+                return select(A, q + 1, k, r)
+            else:
+                return select(A, p, k, q - 1)
+
+    n = len(A)
+    if n%2 == 1:
+        return select(A, 0, n//2, n-1)
+    else:
+        return (select(A, 0, n//2 - 1, n-1) + select(A, 0, n//2, n-1))/2
