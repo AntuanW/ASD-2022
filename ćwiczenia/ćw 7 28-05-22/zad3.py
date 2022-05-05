825de3667c5d7cead252254b7e2985c4575b8307
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-05 02:23:52 +0200

    ex7/zad3 ladowanie promu ptegami 2

--- ćwiczenia/ćw 7 28-05-22/zad3.py
+++ ćwiczenia/ćw 7 28-05-22/zad3.py
@@ -1,2 +1,12 @@
-#todo
-
+def fill(T, K):
+    T.sort(reverse = True)
+    i = 0
+    n = len(T)
+    curr = K
+    res = []
+    while i < n:
+        if curr >= T[i]:
+            res.append(T[i])
+            curr -= T[i]
+        i += 1
+    return res
