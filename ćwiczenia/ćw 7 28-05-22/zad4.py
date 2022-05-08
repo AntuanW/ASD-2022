11cd02b93c79c6ed7584a1d9afb728b4c97e2102
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-08 15:57:27 +0200

    rozwiązanie wież

--- ćwiczenia/ćw 7 28-05-22/zad4.py
+++ ćwiczenia/ćw 7 28-05-22/zad4.py
@@ -1,2 +1,7 @@
-#todo
-
+"""
+F[T] - min liczba klocków które należy ukraść aby ułożyć wieżę o wysokości >= T
+Wartościami T są kolejne poziomy każdej klockowej wieży + 1.
+Aby obliczyć F[T], należy najpierw brać z każdej wieży największe klocki tak aby każda z nich była wysokości < T.
+Jeżeli uzyskana w ten sposób wieża nadal będzie za niska, kradniemy maksimum globalne z klocków tak długo aż nasza wieża spełni warunek.
+Rozwiązaniem zadania jest minimum po wszystkich wartościach T.
+"""
