4a664a96381ffc95cc332238d2a4fa038d181411
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-08 16:14:00 +0200

    cw 7 zad5

--- ćwiczenia/ćw 7 28-05-22/zad5.py
+++ ćwiczenia/ćw 7 28-05-22/zad5.py
@@ -1,2 +1,20 @@
-#todo
+def ad_1(T, L):
 
+    n = len(T)
+    curr = 0
+    res = []
+    while curr + L < n - 1:
+        counter = L
+        while not T[curr + counter] and counter >= 0:
+            counter -= 1
+        curr += counter
+        res.append(curr)
+    return res
+
+def ad_2(T, L):
+    """
+    Patrzymy na każdą stacje w naszym zasięgu. Jezeli ktoras stacja ma tansze paliwo, tankujemy z obecnej tyle aby tam dojachac
+    i jedziemy. Jezeli juz jestesmy na najtanszej stacji to tankujemy do pelna i jedziemy na nastepnej najtanszej.
+    Calosc powtarzamy az dojedziemy do konca.
+    """
+    return 0
