8f2ca50571eb570c522517795f36a7cb0fdb8fd4
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-09 13:59:21 +0200

    cw 6 zad 6

---
+++ ćwiczenia/ćw 6 21-04-22/zad6.py
@@ -0,0 +1,9 @@
+"""
+f(v) - maksymalna ścieżka wychodząca z węzła v.
+C[v] - wartośc wierzchołka v
+k[v] - dzieci wierzchołka v
+f(v) - max(f(k) + C[v] | k £ C[v])
+Po obliczeniu funkcji f dla kazdego wierzcholka przechodze po calym drzewie i sprawdzam parami wiecholki majace wspolnego rodzica.
+Szukam najlepszej sciezki o wartosci best
+best = max(best, f(a) + f(b) - C[i], f(a), f(b)), gdzie a, b - para wierzcholkow wychodzaca z węzła
+"""
