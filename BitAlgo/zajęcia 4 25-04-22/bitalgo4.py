7a013dbc95a28b34a18aa94070170287f5a9a7eb
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-10 01:08:39 +0200

    initial bit

--- BitAlgo/zajęcia 4 25-04-22/bitalgo4.py
+++ BitAlgo/zajęcia 4 25-04-22/bitalgo4.py
@@ -1,1 +1,32 @@
 #todo
+def zad_1():
+    return 0
+
+
+def zad_2():
+    return 0
+
+
+def zad_3():
+    return 0
+
+
+def zad_4():
+    return 0
+
+
+def zad_5():
+    return 0
+
+
+def zad_6():
+    return 0
+
+
+def zad_7():
+    return 0
+
+
+if __name__ == '__main__':
+
+    pass
