aa0e3675b90c7a752d0301896bad3bcaf215ee68
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-17 18:45:46 +0200

    zad1 cw 8

--- ćwiczenia/ćw 8 05-05-22/zad1.py
+++ ćwiczenia/ćw 8 05-05-22/zad1.py
@@ -1,1 +1,39 @@
 #todo
+from collections import deque
+
+def czy_dwudzielny(G):
+    n = len(G)
+    Q = deque()
+    visited = [0]*n
+    visited[0] = 1
+    Q.append(0)
+    while not len(Q) == 0:
+        u = Q.popleft()
+        colour = visited[u]
+        for v in G[u]:
+            if visited[v] == colour:
+                return False
+            if visited[v] == 0:
+                visited[v] = -1 * colour
+                Q.append(v)
+    return True
+
+
+def liczba_spojnych_skladowych(G):
+
+    def DFS_visit(u):
+        nonlocal G, visited
+        visited[u] = True
+        for v in G[u]:
+            if not visited[v]:
+                DFS_visit(v)
+
+    n = len(G)
+    counter = 0
+    visited = [False]*n
+    for i in range(n):
+        if not visited[i]:
+            counter += 1
+            DFS_visit(i)
+
+    return counter
