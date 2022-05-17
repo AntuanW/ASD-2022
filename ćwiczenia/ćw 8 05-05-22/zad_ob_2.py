d01d94daf4f82afa302400c7d6182bc645ea397f
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-18 00:56:01 +0200

    zad obowiazkowe 2 cw 8

--- ćwiczenia/ćw 8 05-05-22/zad_ob_2.py
+++ ćwiczenia/ćw 8 05-05-22/zad_ob_2.py
@@ -1,1 +1,14 @@
-#todo
+def cykl_na_cztery(G):
+    n = len(G)
+    if n < 4:
+        return False
+
+    for i in range(n-1):
+        for j in range(i+1, n):
+            cycle = []
+            for k in range(n):
+                if G[i][k] == G[j][k] == 1:
+                    cycle.append(k)
+                if len(cycle) == 2:
+                    return [i, cycle[0], j, cycle[1]]
+    return False
