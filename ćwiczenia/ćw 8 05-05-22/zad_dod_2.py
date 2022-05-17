d3fec8f9ce56518d509c0dafcaee19457e3d3301
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-18 01:30:56 +0200

    zad dodatkowe 2 cw 8

--- ćwiczenia/ćw 8 05-05-22/zad_dod_2.py
+++ ćwiczenia/ćw 8 05-05-22/zad_dod_2.py
@@ -1,1 +1,18 @@
-#todo
+def czy_nieskierowany(G):
+    n = len(G)
+
+    M = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in G[i]:
+            M[i][j] = 1
+
+    T = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            T[i][j] = M[j][i]
+
+    for i in range(n):
+        for j in range(n):
+            if T[i][j] != M[i][j]:
+                return False
+    return True
