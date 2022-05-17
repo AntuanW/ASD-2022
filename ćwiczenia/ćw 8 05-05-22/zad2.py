ca1942ed1921fe5a3bd704617b11ffc79715aa71
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-17 19:34:10 +0200

    zad2 cw 8

--- ćwiczenia/ćw 8 05-05-22/zad2.py
+++ ćwiczenia/ćw 8 05-05-22/zad2.py
@@ -1,1 +1,27 @@
-#todo
+# uniwersalne ujście
+# reprezentacja macierzowa
+def ujscie(G):
+    n = len(G)
+    row = 0
+    col = 0
+    while row < n and col < n:
+        if G[row][col] == 0:
+            col += 1
+        else:
+            row += 1
+    if col == n:
+        candidate = row
+    else:
+        candidate = col
+
+    for i in range(n):
+        if i != candidate:
+            if G[candidate][i] != 0:
+                return False
+            if G[i][candidate] != 1:
+                return False
+        else:
+            if G[i][i] != 0:
+                return False
+
+    return candidate
