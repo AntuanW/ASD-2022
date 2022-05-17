cd98570a8c10a82b8b9ac175181c04944d458eac
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-17 19:48:13 +0200

    zad3 cw 8

--- ćwiczenia/ćw 8 05-05-22/zad3.py
+++ ćwiczenia/ćw 8 05-05-22/zad3.py
@@ -1,1 +1,32 @@
 #todo
+from collections import deque
+def path(G, start, meta):
+
+    def BFS(s):
+        nonlocal G, parent, visited
+        Q = deque()
+        visited[s] = True
+        Q.append(s)
+        while len(Q) != 0:
+            u = Q.popleft()
+            for v in G[u]:
+                if not visited[v]:
+                    visited[v] = True
+                    parent[v] = u
+                    Q.append(v)
+
+    n = len(G)
+    parent = [None]*n
+    visited = [False]*n
+
+    BFS(start)
+    res = []
+
+    if not visited[meta]:
+        return res
+
+    curr = meta
+    while curr != None:
+        res.append(curr)
+        curr = parent[curr]
+    return res
