a2144db3f9fc6b82ce5ec4f2bed05b955bfdf6de
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-19 16:07:17 +0200

    initial bitalgo7

--- BitAlgo/zajęcia 7 16-05-22/bitalgo7.py
+++ BitAlgo/zajęcia 7 16-05-22/bitalgo7.py
@@ -1,1 +1,27 @@
 #todo
+from collections import deque
+
+
+def zad1():
+    return 0
+
+
+def zad2():
+    return 0
+
+
+def zad3():
+    return 0
+
+
+def zad4():
+    return 0
+
+
+def zad5():
+    return 0
+
+
+if __name__ == "__main__":
+
+    pass
