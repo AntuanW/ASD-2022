30041c1dfc0cb00af13d0f230ca436bd09d0b8d1
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-05-20 13:51:57 +0200

    MST

--- wykłady/wykład 11 11-05-22/MST_minimal_spanning_tree.py
+++ wykłady/wykład 11 11-05-22/MST_minimal_spanning_tree.py
@@ -1,1 +1,50 @@
-#todo
+from queue import PriorityQueue
+
+
+class Node:
+    def __init__(self, value):
+        self.parent = self
+        self.value = value
+        self.rank = 0
+
+
+def MST(G):     # reprezentacja listowa
+
+    def find(x):
+        if x.parent != x:
+            x.parent = find(x.parent)
+        return x.parent
+
+    def union(x, y):
+        x = find(x)
+        y = find(y)
+        if x == y:
+            return
+        if x.rank > y.rank:
+            y.parent = x
+        else:
+            x.parent = y
+            if x.rank == y.rank:
+                y.rank += 1
+        return
+
+    n = len(G)
+    vertex = [None]*n
+    for i in range(n):
+        vertex[i] = Node(i)
+
+    Q = PriorityQueue()
+    for i in range(n):
+        for u in G[i]:
+            Q.put((u[1], i, u[0]))
+
+    mst = []
+    while not Q.empty():
+        weight, u, v = Q.get()
+        u = vertex[u]
+        v = vertex[v]
+        if find(u) != find(v):
+            mst.append((u.value, v.value))
+            union(u, v)
+
+    return mst
