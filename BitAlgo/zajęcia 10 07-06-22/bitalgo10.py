fcee999674d084c0138adf5eb0765d722b66df4f
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-14 18:56:54 +0200

    initial

--- BitAlgo/zajęcia 10 07-06-22/bitalgo10.py
+++ BitAlgo/zajęcia 10 07-06-22/bitalgo10.py
@@ -1,21 +1,43 @@
 # todo
 def zad1():
+    """
+    Tworzymy graf, dla kazdej osoby puszczamy BFS.
+    """
     pass
 
 
 def zad2():
+    """
+    Tworzymy graf dwudzielny.
+    Jednym zbiorem wierzcholkow sa aplikacje a drugim komputery.
+    aplikacje lacza sie z komputerami
+    tworzymy super zrodlo i super ujscie
+    robimy przeplywem
+    wszystkie krawedzie maja pojemnosc 1
+    krawedzie ze zrodla do aplikacji maja pojemnosc rowna liczbie uzytkownikow, ktorzy chca z niej korzystac
+    """
     pass
 
 
 def zad3():
+    """
+
+    """
     pass
 
 
 def zad4():
+    """
+    Wierzcholkami w grafie sa osady. Budujac graf wagami krawędzi sa odległości.
+    Budujemy MST i patrzymy na ostatnią dodaną krawędź - jej waga to szukany zasięg
+    """
     pass
 
 
 def zad5():
+    """
+
+    """
     pass
 
 
