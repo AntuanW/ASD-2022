14b277b1960936e81be0fe6809fb11e8d601c144
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-14 16:57:00 +0200

    bitalgo9

--- BitAlgo/zajęcia 9 31-05-22/bitalgo9.py
+++ BitAlgo/zajęcia 9 31-05-22/bitalgo9.py
@@ -1,1 +1,65 @@
 # todo
+def zad1():
+    """
+    Pomnaażamy wierzchołki, Nastepnie Dijkstra. (Podobnie jak na ćwiczeniach)
+    """
+    pass
+
+
+def zad2():
+    """
+    zadanie 1 z prezentacji
+    Kazdy wierzcholek rozbijamy na szereg wierzcholkow.
+    np D1 oznacza ze dojechalismy do D i mamy 1 l paliwa
+    Na kazdej stacji łaczymy wszystkie wierzcholki z danej literki
+    """
+    pass
+
+
+def zad3():
+    """
+    Funkcje min() zamnieniamy na if, tworzymy i wypelniamy tablice parent.
+    """
+    pass
+
+
+def zad4():
+    """
+    a) Szukamy sekwencji o najkorzystniejszym iloczynie. korzystamy z triku na logarytmowanie kazdej krawędzi i Dijkstra.
+    b) kazdy kurs zamieniamy na -log kurs, nastepnie bellman ford i szykamy ujemnych cykli
+    """
+    pass
+
+
+def zad5():
+    """
+    Kazdy wierzcholek rozbijamy na 2.
+    Jeden informuje nas ze przyszlismy do niego nie wykorzystujac krawedzi z E, drugi mowi ze juz to zrobilismy
+    """
+    pass
+
+
+def zad6():
+    """
+    Po kolei usuwamy kazda krawedz, nastepnie szukamy najkrotsza sciazke miedzy koncami tej krawedzi.
+    """
+    pass
+
+
+def zad7():
+    """
+    Modyfikacja relaksacji.
+    """
+    pass
+
+
+def zad8():
+    """
+    XD
+    """
+    pass
+
+
+if __name__ == "__main__":
+
+    pass
