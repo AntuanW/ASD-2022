106d9b7f95755e78b378293f3311078885809438
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-14 16:59:52 +0200

    initial

---
+++ BitAlgo/zajęcia 10 07-06-22/bitalgo10.py
@@ -0,0 +1,28 @@
+# todo
+def zad1():
+    pass
+
+
+def zad2():
+    pass
+
+
+def zad3():
+    pass
+
+
+def zad4():
+    pass
+
+
+def zad5():
+    pass
+
+
+def zad6():
+    pass
+
+
+if __name__ == "__main__":
+
+    pass
