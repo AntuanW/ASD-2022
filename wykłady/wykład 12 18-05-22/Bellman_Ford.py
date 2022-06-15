e15815e9eadf0eca25e91b5a2c9a6ea1ea2be598
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-15 13:40:09 +0200

    Bellman Ford

--- wykłady/wykład 12 18-05-22/Bellman_Ford.py
+++ wykłady/wykład 12 18-05-22/Bellman_Ford.py
@@ -1,1 +1,25 @@
-#todo
+def bellman_ford(G, W, s):
+
+    def relax(u, v):
+        nonlocal parent, G, dist, W
+        if dist[v] > dist[u] + W[u][v]:
+            dist[v] = dist[u] + W[u][v]
+            parent[v] = u
+
+    n = len(G)
+    dist = [float('inf')]*n
+    dist[s] = 0
+    parent = [None]*n
+
+    for _ in range(n-1):
+        for u in range(n):
+            for v in G[u]:
+                relax(u, v)
+
+    for u in range(n):
+        for v in G[u]:
+            if dist[v] > dist[u] + W[u][v]:
+                return None
+
+    return parent
+
