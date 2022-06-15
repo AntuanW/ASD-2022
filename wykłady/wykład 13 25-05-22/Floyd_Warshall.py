c35b3ce36da4aebd4f39905c01bb453977805365
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-15 14:17:51 +0200

    Floyd-Warshall

--- wykłady/wykład 13 25-05-22/Floyd_Warshall.py
+++ wykłady/wykład 13 25-05-22/Floyd_Warshall.py
@@ -1,1 +1,13 @@
-#todo
+def Floyd_Warshall(G):
+    n = len(G)
+    M = [[float('inf') for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j, w in G[i]:
+            M[i][j] = w
+        M[i][i] = 0
+
+    for t in range(n):
+        for u in range(n):
+            for v in range(n):
+                M[u][v] = min(M[u][v], M[u][t] + M[t][v])
+    return M
