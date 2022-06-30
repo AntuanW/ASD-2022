a7de9c09406f2d2b3a31b22ae149715a561c8b84
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-06-30 17:09:39 +0200

    kol 3a O(E log n)

--- KOLOKWIUM/kol3a/kol3a.py
+++ KOLOKWIUM/kol3a/kol3a.py
@@ -1,52 +1,47 @@
 #Antoni Wójcik
 """
 Opis algorytmu:
-Algorytm bazuje na algorytmie Dijkstry.
-Najpierw przygotowuję graf dodając krawędzie miedzy osobliwościami z wagą 0.
-Następnie znajduję najkrótszą ścieżkę między wierzchołkami a i b uzywając algorytmu Dijkstry.
-Złożoność czasowa: O(n^2)
+1. Tworze graf g na podstawie listy krawedzi
+2. Obliczam odleglosci od wierzholka a i od wierzcholka b za pomoca dijkstry
+3. Rozwiazaniem bedzie albo odlegosc od a do b albo odleglosc od a do najblizszej supergwiazdy + odl z b do najblizszej supergwiazdy
 """
 from kol3atesty import runtests
 import heapq
 
 def spacetravel( n, E, S, a, b ):
-    def dijkstra(G, W, s):
-        def relax(u, v):
-            nonlocal parent, G, dist, W, Q
-            if dist[v] > dist[u] + W[u][v]:
-                dist[v] = dist[u] + W[u][v]
-                parent[v] = u
+    def dijkstra(G, s):
+        def relax(u, v, w):
+            nonlocal G, dist, Q
+            if dist[v] > dist[u] + w:
+                dist[v] = dist[u] + w
                 heapq.heappush(Q,(dist[v], v))
         n = len(G)
         dist = [float('inf')] * n
         dist[s] = 0
-        parent = [None] * n
         Q = []
         heapq.heappush(Q, (0, s))
         while len(Q) != 0:
-            u = heapq.heappop(Q)
-            u = u[1]
+            u = heapq.heappop(Q)[1]
             for v in G[u]:
-                relax(u, v)
+                relax(u, v[0], v[1])
         return dist
 
     G = [[] for _ in range(n)]
-    W = [[-1 for _ in range(n)] for _ in range(n)]
-    for i, j, k in E:
-        W[i][j] = k
-        W[j][i] = k
-    for i in range(len(S)):
-        for j in range(len(S)):
-            if S[i] != S[j]:
-                W[S[i]][S[j]] = 0
-                W[S[j]][S[i]] = 0
-    for i in range(n):
-        for j in range(n):
-            if W[i][j] != -1:
-                G[i].append(j)
-    dist = dijkstra(G, W, a)
-    if dist[b] != float('inf'):
-        return dist[b]
+    for i, j, w in E:
+        G[i].append((j, w))
+        G[j].append((i, w))
+    dist_from_a = dijkstra(G, a)
+    dist_from_b = dijkstra(G, b)
+    curr_dist = dist_from_a[b]
+    star_a = float('inf')
+    star_b = float('inf')
+    for x in S:
+        star_a = min(star_a, dist_from_a[x])
+        star_b = min(star_b, dist_from_b[x])
+
+    curr_dist = min(curr_dist, star_a + star_b)
+    if curr_dist != float('inf'):
+        return curr_dist
     return None
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
